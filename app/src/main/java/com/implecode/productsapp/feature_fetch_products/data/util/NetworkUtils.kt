9c0f83fa6cf3d457f8455fca8aedbb783e03a036
AumaidKh <aumaidm.m.c@gmail.com>
2022-09-30 07:49:54 +0530

    Added Progress Bar

--- app/src/main/java/com/implecode/productsapp/feature_fetch_products/data/util/NetworkUtils.kt
+++ app/src/main/java/com/implecode/productsapp/feature_fetch_products/data/util/NetworkUtils.kt
@@ -7,20 +7,33 @@ import retrofit2.Response
 
 object NetworkUtils {
 
+    /**
+     * Handling the api errors here
+     * Function check for internet connectivity an shows the corresponding error message
+     * */
     suspend fun <T : Any> makeApiRequest(
         isConnectedToInternet: Boolean,
         onRequest: suspend () -> Response<T>
     ): ResponseWrapper<T> =
         withContext(Dispatchers.IO) {
+            /**
+             * Internet Not Detected, don't make the api call*/
             if (!isConnectedToInternet) {
                 return@withContext ResponseWrapper.Error(ApiError.InternetConnectionError())
             }
             safeApiCall {
                 with(onRequest()) {
                     when {
+                        /**
+                         * Api Call Succeeded
+                         * */
                         isSuccessful && body() != null -> {
                             ResponseWrapper.Success(body() as T)
                         }
+                        /**
+                         * Server Error Happened, will be showing a message
+                         * directly coming from the server
+                         * */
                         else -> {
                             ResponseWrapper.Error(
                                 ApiError.ServerError(
